930a1ade1b2ffd89bd41c60d0999f536553b1d1a
Alexey Badenkov <alexey.badenkov@gmail.com>
2025-04-06 23:13:41 +0400

    configured devenv

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = [
+    pkgs.libffi
+
+    pkgs.inotify-tools
+    pkgs.git
+    pkgs.watchman
+
+    pkgs.libyaml.dev
+    pkgs.libxml2.dev
+    pkgs.openssl.dev
+
+    pkgs.vips # for active_storages
+    pkgs.vips.dev # for active_storages
+
+    pkgs.curl.dev # libcurl?
+  ];
+
+  languages.ruby.enable = true;
+  languages.ruby.versionFile = ./.ruby-version;
+  languages.ruby.bundler.enable = true;
+
+  processes = {
+  } // lib.optionalAttrs (!config.devenv.isTesting) {
+    web.exec = "bin/rails server";
+    css.exec = "bin/rails tailwindcss:watch";
+  };
+
+  enterTest = ''
+    bundle install
+    bin/rails test
+  '';
+}
