ab968fbb3ea7c3ccbfca7221f38570a7de938f9c
Alexey Badenkov <alexey.badenkov@gmail.com>
2025-04-07 14:02:48 +0400

    move report to concern

--- app/models/monitored_endpoint.rb
+++ app/models/monitored_endpoint.rb
@@ -1,4 +1,6 @@
 class MonitoredEndpoint < ApplicationRecord
+  include Report
+
   URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
   MINUTE = 60
   HOUR = MINUTE * 60
@@ -18,7 +20,6 @@ class MonitoredEndpoint < ApplicationRecord
 
   before_save :calc_launch_time
   after_commit :launch_check_async, if: -> { in_progress? && status_previously_changed? }
-  after_commit :flush_cache, if: -> { ready? && status_previously_changed? }
 
   def launch_check_async
     LaunchCheckJob.perform_later(self)
@@ -43,61 +44,6 @@ class MonitoredEndpoint < ApplicationRecord
       self.checks.create!(latency: latency, response_code: response_code)
       self.ready!
     end
-  end
-
-  def average_response_time
-    checks.
-      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
-      average(:latency)
-  end
-
-  def total_checks_counts
-    checks.
-      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
-      count
-  end
-
-  def incedent_counts
-    checks.
-      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
-      count("CASE WHEN monitored_endpoint_checks.response_code NOT BETWEEN 200 AND 399 THEN 1 END")
-  end
-
-  def degraded_counts
-    checks.
-      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
-      joins(:monitored_endpoint).
-      count <<~SQL
-              CASE
-              WHEN (monitored_endpoint_checks.response_code BETWEEN 200 AND 399)
-              AND  (monitored_endpoint_checks.latency > monitored_endpoints.threshold)
-              THEN
-                1
-              END
-            SQL
-  end
-
-  def cached_average_response_time
-    Rails.cache.fetch([self.class.name, id, "average_response_time"]) { average_response_time }
-  end
-
-  def cached_total_checks_counts
-    Rails.cache.fetch([self.class.name, id, "total_checks_counts"]) { total_checks_counts }
-  end
-
-  def cached_incedent_counts
-    Rails.cache.fetch([self.class.name, id, "incedent_counts"]) { incedent_counts }
-  end
-
-  def cached_degraded_counts
-    Rails.cache.fetch([self.class.name, id, "degraded_counts"]) { degraded_counts }
-  end
-
-  def flush_cache
-    Rails.cache.delete([self.class.name, id, "average_response_time"])
-    Rails.cache.delete([self.class.name, id, "total_checks_counts"])
-    Rails.cache.delete([self.class.name, id, "incedent_counts"])
-    Rails.cache.delete([self.class.name, id, "degraded_counts"])
   end
 
   private
---
+++ app/models/monitored_endpoint/report.rb
@@ -0,0 +1,62 @@
+module MonitoredEndpoint::Report
+  extend ActiveSupport::Concern
+
+  included do
+    after_commit :flush_cache, if: -> { ready? && status_previously_changed? }
+  end
+
+  def average_response_time
+    checks.
+      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
+      average(:latency)
+  end
+
+  def total_checks_counts
+    checks.
+      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
+      count
+  end
+
+  def incedent_counts
+    checks.
+      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
+      count("CASE WHEN monitored_endpoint_checks.response_code NOT BETWEEN 200 AND 399 THEN 1 END")
+  end
+
+  def degraded_counts
+    checks.
+      group_by_day(:created_at, reverse: true, range: (Date.today - 6)..Date.today).
+      joins(:monitored_endpoint).
+      count <<~SQL
+              CASE
+              WHEN (monitored_endpoint_checks.response_code BETWEEN 200 AND 399)
+              AND  (monitored_endpoint_checks.latency > monitored_endpoints.threshold)
+              THEN
+                1
+              END
+            SQL
+  end
+
+  def cached_average_response_time
+    Rails.cache.fetch([self.class.name, id, "average_response_time"]) { average_response_time }
+  end
+
+  def cached_total_checks_counts
+    Rails.cache.fetch([self.class.name, id, "total_checks_counts"]) { total_checks_counts }
+  end
+
+  def cached_incedent_counts
+    Rails.cache.fetch([self.class.name, id, "incedent_counts"]) { incedent_counts }
+  end
+
+  def cached_degraded_counts
+    Rails.cache.fetch([self.class.name, id, "degraded_counts"]) { degraded_counts }
+  end
+
+  def flush_cache
+    Rails.cache.delete([self.class.name, id, "average_response_time"])
+    Rails.cache.delete([self.class.name, id, "total_checks_counts"])
+    Rails.cache.delete([self.class.name, id, "incedent_counts"])
+    Rails.cache.delete([self.class.name, id, "degraded_counts"])
+  end
+end
